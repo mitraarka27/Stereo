00d19abb62e6d7c7eeb0c2b58669df54524fcaf5
mitraarka27 <32750328+mitraarka27@users.noreply.github.com>
2017-11-30 17:06:56 -0600

    Update misr_m2.f90

--- misr_m2.f90
+++ misr_m2.f90
@@ -13,15 +13,15 @@ program misr_m2
 	status = nf90_inquire_dimension(ncid,2,len = dimension(2))
 	status = nf90_inquire_dimension(ncid,3,len = dimension(3))
 	
-nx = dimension(2)
-	ny = dimension(3)
+   nx = dimension(2)
+   ny = dimension(3)
 	
 !   Check for angular information
 !   zeniths represented as cos_inverse(mu) and azimuths as phi
     
 	status = nf90_inq_varid(ncid,'intensityMus',muid)
 	if(status .ne. nf90_noerr) stop 'Zenith angles not found'
-    status = nf90_inq_varid(ncid,'intensityPhis',phiid)
+        status = nf90_inq_varid(ncid,'intensityPhis',phiid)
 	if(status .ne. nf90_noerr) stop 'Azimuth angles not found'
 	
 !   Allocating memory for storing the angular information
@@ -33,11 +33,11 @@ nx = dimension(2)
 
 	status = nf90_get_var(ncid,muid,mu)
 	if(status .ne. nf90_noerr) stop 'Zenith angles not found in file'
-    status = nf90_get_var(ncid,phiid,phi)
+        status = nf90_get_var(ncid,phiid,phi)
 	if(status .ne. nf90_noerr) stop 'Azimuth angles not found in file'
 	
 !   Now calculate reference zenith and comparison zeniths
 
     Ref_zenith = acos(180.0/pi*mu(Ref_File_ID))
-	Comp_zenith_1 = acos(180.0/pi*mu(Comp_File_1_ID))
-	Comp_zenith_2 = acos(180.0/pi*mu(Comp_File_2_ID))
+    Comp_zenith_1 = acos(180.0/pi*mu(Comp_File_1_ID))
+    Comp_zenith_2 = acos(180.0/pi*mu(Comp_File_2_ID))
