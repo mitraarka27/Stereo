8305313e896ec464fee6c4d707bf9d806da1c783
mitraarka27 <32750328+mitraarka27@users.noreply.github.com>
2017-12-08 15:29:12 -0600

    Update misr_m2.f90

--- misr_m2.f90
+++ misr_m2.f90
@@ -1,51 +1,59 @@
 program misr_m2
-  use netcdf
-  implicit none
-  
-  write(*,*) 'Opening I3RC output file...'
+   use netcdf
+   implicit none
+!--**************************************-Variable definitions-***********************************************************
+
+
+
+!--**************************************-Reading from I3RC output-*******************************************************
+    write(*,*) 'Opening I3RC output file...'
   
     status = nf90_open(netcdf_file,0,ncid)
-    if(status .ne. nf90_noerr) stop 'File Error'
     status = nf90_inq_varid(ncid,'intensity',varid)
-    if(status .ne. nf90_noerr) stop 'Variable not found'
-	
-    status = nf90_inquire_dimension(ncid,1,len = dimension(1))
-    status = nf90_inquire_dimension(ncid,2,len = dimension(2))
-    status = nf90_inquire_dimension(ncid,3,len = dimension(3))
-	
-    nx = dimension(1)
-    ny = dimension(2)
-	
+    status = nf90_inquire_dimension(ncid,1,len = length(1))
+    status = nf90_inquire_dimension(ncid,2,len = length(2))
+    status = nf90_inquire_dimension(ncid,3,len = length(3))
+
 !   Check for angular information
 !   zeniths represented as cos_inverse(mu) and azimuths as phi
     
-	status = nf90_inq_varid(ncid,'intensityMus',muid)
-	if(status .ne. nf90_noerr) stop 'Zenith angles not found'
-        status = nf90_inq_varid(ncid,'intensityPhis',phiid)
-	if(status .ne. nf90_noerr) stop 'Azimuth angles not found'
+    status = nf90_inq_varid(ncid,'intensityMus',muid)
+    status = nf90_inq_varid(ncid,'intensityPhis',phiid)
 	
-!   Allocating memory for storing the angular information
-!   length of angular information = dimension(3)
 
-    allocate(mu(dimension(3)),phi(dimension(3)))
+    nx = length(1)
+    ny = length(2)
+
+!   Allocating memory to store the required radiance data from netcdf file
+!   Allocating memory for storing the angular information. Length of angular information = dimension(3)
+    
+    allocate(FileData(length(1),length(2),length(3)))
+    allocate(RefImage(nx,ny),CompImage1(nx,ny),CompImage2(nx,ny))
+    allocate(mu(length(3)),phi(length(3)))
+    allocate(H(nx,ny),H1(nx,ny),H2(nx,ny))
+
+!   Reading the radiance information from the output file
+
+    status=nf90_get_var(ncid,varid,FileData) 
+    RefImage = FileData(:,:,RefImageNum)
+    CompImage1 = FileData(:,:,CompImageNum1)
+    CompImage2 = FileData(:,:,CompImageNum2)
 	
 !   Reading the angular information off the I3RC output file
 
-	status = nf90_get_var(ncid,muid,mu)
-	if(status .ne. nf90_noerr) stop 'Zenith angles not found in file'
-        status = nf90_get_var(ncid,phiid,phi)
-	if(status .ne. nf90_noerr) stop 'Azimuth angles not found in file'
-	
+    status = nf90_get_var(ncid,muid,mu)
+    status = nf90_get_var(ncid,phiid,phi)
+
 !   Now calculate reference zenith and comparison zeniths
 
-    Ref_zenith = acos(180.0/pi*mu(Ref_File_ID))
-    Comp_zenith_1 = acos(180.0/pi*mu(Comp_File_1_ID))
-    Comp_zenith_2 = acos(180.0/pi*mu(Comp_File_2_ID))
+    Ref_zenith = acos(180.0/pi*mu(RefImageNum))
+    Comp_zenith_1 = acos(180.0/pi*mu(CompImageNum1))
+    Comp_zenith_2 = acos(180.0/pi*mu(CompImageNum2))
 
 !  Now let us initialize the height vectors H1, H2 and the cumulative height vector H with garbage values(-666)
-   H(:,:) = -666
-   H1(:,:) = -666
-   H2(:,:) = -666
+    H(:,:) = -666
+    H1(:,:) = -666
+    H2(:,:) = -666
    
 !  Now calculate the sizes of the search area box for 1st comparison image
 !  Refer to MISR Level 2 Cloud Product Algorithm Theoretical Basis (Page 17), JPL D-73327
@@ -65,10 +73,30 @@ program misr_m2
 !  Setting the number of steps in the along-track(y) and cross-track(x) directions
     Nc = abs(dxmax-dxmin) + 1
     Na = abs(dcmax-dcmin) + 1
-    max_patches = Nc * Na 
+    max_patches = Nc * Na + 1
     
-!  Now calculate cloud top heights using the stereo code
+!  Now calculate cloud top heights using the stereo code for 1st Comparison Image
     
-    H1 = stereo( , , , )
+    H1 = stereo(RefImage(:,:), CompImage1(:,:), Ref_zenith, Comp_Zenith_1)
     
-!  
+!  Now calculate the sizes of the search area box for 2nd comparison image
+
+    dtan = tan(Comp_Zenith_2 * pi/180) - tan(Ref_zenith * pi/180)
+    if (dtan.ge.0) then
+    dxmin = int((hmin * dtan - abs(vmax * dt))/pixel_size)-1
+    dxmax = int((hmax * dtan + abs(vmax * dt))/pixel_size)+1
+    else 
+    dxmin = int((hmax * dtan - abs(vmax * dt))/pixel_size)-1
+    dxmax = int((hmin * dtan + abs(vmax * dt))/pixel_size)+1
+    end if
+    dymin = int(-abs(vmax * dt)/pixel_size)-1
+    dymax = int(abs(vmax * dt)/pixel_size)+1
+    
+!  Setting the number of steps in the along-track(y) and cross-track(x) directions again 
+    Nc = abs(dxmax-dxmin) + 1
+    Na = abs(dcmax-dcmin) + 1
+    max_patches = Nc * Na + 1
+    
+!  Now calculate cloud top heights using the stereo code for 2nd comparison image
+    
+    H1 = stereo(RefImage(:,:), CompImage2(:,:), Ref_zenith, Comp_Zenith_2)  
